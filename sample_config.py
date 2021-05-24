d14a820718cfa93c13d0705e08c233c893de9eb3
Dlaize Kohli <dakshkohli23@gmail.com>
2021-05-24 21:30:20 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -4,18 +4,18 @@ import os
 
 class Config(object):    
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
     # Get these values from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID"))
+    APP_ID = int(os.environ.get("APP_ID", ""))
 
     API_HASH = os.environ.get("API_HASH")
 
     # Array to store users who are authorized to use the bot
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
     
-    # Banned Unwanted Members..
-    BANNED_USERS = os.environ.get("BANNED_USERS")
+    # Update channel for Force Subscribe
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
 
     # Channels to forward the formatted video (Optional, Prefix: "-100", Bot should be an admin of the channels)
     CHANNEL1_ID = os.environ.get("CHANNEL1_ID")
